3e809e82b9281731272b7e60fbd21247f8063cf5
AndreLz <andre.luizpvieira@gmail.com>
2020-03-28 16:31:46 -0400

    trocando banco de producao

--- src/plugins/parse.js
+++ src/plugins/parse.js
@@ -18,11 +18,16 @@ if (process.env.NODE_ENV === 'development') {
     parse.serverURL = "https://parseapi.back4app.com";
 
 } else {
-    // production conta oficial
-    parse.initialize(
-        "KCgObmpYt2Qvup2XD9YOVjFA5Kglh6fAra6U3nyS",
-        "Tmjk5im5qT6iKhalwKawh4rtAVpYc6wNY3v3m4Oo"
+     // conta antiga
+     parse.initialize(
+        "xXJL5MWBPATUGL28PN3T8e5LcIotlGk0O731bUKh",
+        "RKI8zS9wv9nGFNJb0mG3keU6Lc1zRIUA1M4OCsPF"
     );
+    // production conta oficial
+    // parse.initialize(
+    //     "KCgObmpYt2Qvup2XD9YOVjFA5Kglh6fAra6U3nyS",
+    //     "Tmjk5im5qT6iKhalwKawh4rtAVpYc6wNY3v3m4Oo"
+    // );
     parse.serverURL = "https://parseapi.back4app.com";
 }
 
